3742081ed558d42cfffba2248bbe34c1a22b4399
Clara <cskarler@gmail.com>
2024-04-20 18:22:14 +0200

    Add project files.

---
+++ WebApi/Controllers/SubscribeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SubscribeController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> SubscribeAsync()
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> UnsubscribeAsync()
+    {
+        return Ok();
+    }
+}
